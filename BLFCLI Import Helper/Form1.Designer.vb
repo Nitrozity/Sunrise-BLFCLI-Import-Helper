cd756cd58a385a6019e40931128eba9e1f23026b
Nitrozity <111669572+Nitrozity@users.noreply.github.com>
2025-04-10 21:03:47 -0400

    Add files via upload

---
+++ BLFCLI Import Helper/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.BuildVer = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Load Folder"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 39)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.ScrollAlwaysVisible = True
+        Me.ListBox1.Size = New System.Drawing.Size(268, 264)
+        Me.ListBox1.TabIndex = 3
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(252, 308)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(28, 19)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "0"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(143, 10)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(113, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Set Sub Directory"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(262, 15)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(15, 14)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Blue
+        Me.Label2.Location = New System.Drawing.Point(12, 308)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "More Commands"
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(47, 329)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(196, 31)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Build Title Storage"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(47, 366)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(196, 31)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "Build Title Storage Config"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(48, 410)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Build:"
+        '
+        'BuildVer
+        '
+        Me.BuildVer.AutoSize = True
+        Me.BuildVer.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BuildVer.ForeColor = System.Drawing.Color.Blue
+        Me.BuildVer.Location = New System.Drawing.Point(79, 410)
+        Me.BuildVer.Name = "BuildVer"
+        Me.BuildVer.Size = New System.Drawing.Size(163, 13)
+        Me.BuildVer.TabIndex = 12
+        Me.BuildVer.Text = "12070.08.09.05.2031.halo3_ship"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(291, 328)
+        Me.Controls.Add(Me.BuildVer)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "BLF CLI Import Helper"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label3 As Label
+    Friend WithEvents BuildVer As Label
+End Class
---
+++ BLFCLI Import Helper/Form1.vb
@@ -0,0 +1,81 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Clear()
+        If Me.FolderBrowserDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+
+            Dim fileInfo As New IO.DirectoryInfo(FolderBrowserDialog1.SelectedPath)
+            Dim usermaps As IO.FileInfo() = fileInfo.GetFiles("*", IO.SearchOption.AllDirectories)
+            Dim GetFileInfo As IO.FileInfo
+
+            For Each GetFileInfo In usermaps
+                ListBox1.Items.Add(GetFileInfo)
+            Next
+        End If
+        Label1.Text = ListBox1.Items.Count
+    End Sub
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        If CheckBox1.Checked = True Then
+            Clipboard.SetText((".\blf_cli.exe title-storage import-variant " + Chr(34) + ".\Blam-Title-Storage\Halo 3\Release\default_hoppers" + Chr(34) + " .\variants\" + My.Settings.subDR.ToString + "\" + ListBox1.GetItemText(ListBox1.SelectedItem) + (" ") + Chr(34) + "Halo 3" + Chr(34) + BuildVer.Text))
+        Else
+            Clipboard.SetText((".\blf_cli.exe title-storage import-variant " + Chr(34) + ".\Blam-Title-Storage\Halo 3\Release\default_hoppers" + Chr(34) + " .\variants\" + ListBox1.GetItemText(ListBox1.SelectedItem) + (" ") + Chr(34) + "Halo 3" + Chr(34) + BuildVer.Text))
+        End If
+    End Sub
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+        Clipboard.SetText(".\blf_cli.exe title-storage build " + Chr(34) + ".\Blam-Title-Storage\Halo 3\Release" + Chr(34) + " .\storage\title\tracked\12070 " + Chr(34) + "Halo 3" + Chr(34) + BuildVer.Text)
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Clipboard.SetText(".\blf_cli.exe title-storage build-config .\storage\title\tracked\12070 " + Chr(34) + ".\Blam-Title-Storage\Halo 3\Release" + Chr(34) + " " + Chr(34) + "Halo 3" + Chr(34) + BuildVer.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim subdir As String
+        subdir = Microsoft.VisualBasic.InputBox("Enter the name of your sub directory located in /variants. If you're not using one, uncheck the checkbox.", "", My.Settings.subDR, 600, 400)
+        My.Settings.subDR = subdir
+        My.Settings.Save()
+        My.Settings.Reload()
+    End Sub
+
+    Private Sub BuildVer_Click(sender As Object, e As EventArgs) Handles BuildVer.Click
+        If BuildVer.Text.Contains("12070") Then
+            Dim rslt As New System.Windows.Forms.DialogResult
+            rslt = MessageBox.Show("Change the build to Halo 3 ODST?", "BLFCLI Import Helper", MessageBoxButtons.YesNo)
+            If rslt = Windows.Forms.DialogResult.Yes Then
+                BuildVer.Text = " 13895.09.04.27.2201.atlas_release"
+            End If
+
+        ElseIf BuildVer.Text.Contains("13895") Then
+            Dim rslt2 As New System.Windows.Forms.DialogResult
+            rslt2 = MessageBox.Show("Change the build to Halo 3?", "BLFCLI Import Helper", MessageBoxButtons.YesNo)
+            If rslt2 = Windows.Forms.DialogResult.Yes Then
+                BuildVer.Text = " 12070.08.09.05.2031.halo3_ship"
+            End If
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        MsgBox(My.Settings.subDR)
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            Button2.Enabled = True
+        Else
+            Button2.Enabled = False
+        End If
+    End Sub
+
+    'Form animation after clicking More Commands:
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Me.Size = New Size(Me.Width, Me.Height + 1)
+    End Sub
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        Timer1.Start()
+    End Sub
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Me.Size = New Size(307, 481) Then
+            Timer1.Enabled = False
+            Label2.Visible = False
+        End If
+    End Sub
+End Class
